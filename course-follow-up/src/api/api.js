57addeb9116adcef53c2a4a3184be5b9efef5e87
jpbr66 <jburgos1000@gmail.com>
2024-06-22 01:31:47 -0600

    .

--- course-follow-up/src/api/api.js
+++ course-follow-up/src/api/api.js
@@ -304,11 +304,14 @@ app.delete('/eliminarCurso', async (req, res) => {
 
       const result = db.query(`DELETE FROM railway.grupoxcurso WHERE idgrupo = ? AND idcurso = ?`, [idGrupo, idCurso]);
 
-      res.status(201).json({ mensaje: 'Curso eliminado correctamente', resultado: result });
-
       if (result.affectedRows === 0) {
-        return res.status(404).json({ error: 'Curso no encontrado en el grupo especificado' });
+        res.status(404).json({ error: 'Curso no encontrado en el grupo especificado' });
       }
+      else{
+        res.status(201).json({ mensaje: 'Curso eliminado correctamente', resultado: result });
+      }
+      
+
   } catch (error) {
     console.error('Error al eliminar el curso:', error);
     res.status(500).json({ error: 'Error al eliminar el curso' });
